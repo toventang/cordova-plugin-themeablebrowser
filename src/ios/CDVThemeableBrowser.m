f8ab58dd9ffec9e4cdd34d97879a9cd4901b23bb
toven <ttw130@gmail.com>
2018-03-16 17:22:19 +0800

    修复 ios 中 sendPluginResult 发送失败的BUG。

--- src/ios/CDVThemeableBrowser.m
+++ src/ios/CDVThemeableBrowser.m
@@ -67,6 +67,8 @@
     NSURL *initUrl;  // initial URL ThemeableBrowser opened with
     NSURL *originalUrl;
 }
+@property (nonatomic,strong) dispatch_source_t timer;
+
 @end
 
 @implementation CDVThemeableBrowser
@@ -104,6 +106,8 @@
               withMessage:@"Close called but already closed."];
         return;
     }
+    
+    dispatch_source_cancel(_timer);
     // Things are cleaned up in browserExit.
     [self.themeableBrowserViewController close];
 }
@@ -162,12 +166,33 @@
 
     [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    NSTimeInterval delayTime=3.0f;
+    NSTimeInterval timeInterval=1.0f;
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_time_t startDelayTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayTime * NSEC_PER_SEC));
+    dispatch_source_set_timer(_timer, startDelayTime, timeInterval * NSEC_PER_SEC, 0.1 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(_timer, ^{
+        CDVPluginResult* pluginResult;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    });
+    dispatch_resume(_timer);
 }
 
 - (void)reload:(CDVInvokedUrlCommand*)command
 {
     if (self.themeableBrowserViewController) {
         [self.themeableBrowserViewController reload];
+    }
+}
+
+- (void)changeButtonImage:(CDVInvokedUrlCommand *)command {
+    if (self.themeableBrowserViewController) {
+        NSInteger buttonIndex = [[command.arguments objectAtIndex:0] integerValue];
+        NSDictionary* buttonProps = [command.arguments objectAtIndex:1];;
+        [self.themeableBrowserViewController changeButtonImage:buttonIndex buttonProps:buttonProps];
     }
 }
 
